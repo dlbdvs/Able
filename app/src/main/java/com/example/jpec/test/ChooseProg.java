6593040e163e62b3ec642a52366d3e5db79e072b
Jpec <jean.paul.bella@hotmail.fr>
2017-01-10 18:36:39 +0100

    rien de ouf

--- app/src/main/java/com/example/jpec/test/ChooseProg.java
+++ app/src/main/java/com/example/jpec/test/ChooseProg.java
@@ -16,9 +16,9 @@ public class ChooseProg extends Activity {
     int compteur_serie=1;
     public boolean isCreated= true;
 
-    protected Exercise exoo1=new RegularPullUps(2,10,60.0);
-    protected Exercise exoo2=new CloseGripPullUps(1,10,60.0);
-    protected Exercise exoo3=new WidePullUps(1,10,60.0);
+    protected Exercise exoo1;
+    protected Exercise exoo2;
+    protected Exercise exoo3;
     protected Exercise exoo4;
     protected Exercise exoo5;
     protected Exercise exoo6;
@@ -36,21 +36,83 @@ public class ChooseProg extends Activity {
         @Override
         public void onClick(View v) {
             switch (v.getId()){
+
                 case R.id.workout1:
-                    Intent i = new Intent(v.getContext(), ProgrammActivity.class);
-                    i.putExtra("exoo1", exoo1);
-                    i.putExtra("exoo2", exoo2);
-                    i.putExtra("exoo3", exoo3);
-                    i.putExtra("exoo4", exoo4);
-                    i.putExtra("exoo5", exoo5);
-                    i.putExtra("exoo6", exoo6);
-                    i.putExtra("exoo7", exoo7);
-                    i.putExtra("exoo8", exoo8);
-                    i.putExtra("compteur_exo", compteur_exo);
-                    i.putExtra("compteur_serie", compteur_serie);
-                    i.putExtra("isCreated", isCreated);
-                    startActivity(i);
+                    exoo1=new CloseGripPullUps(5,10,90.0);
+                    Intent i1 = new Intent(v.getContext(), ProgrammActivity.class);
+                    i1.putExtra("name_workout", "Armstrong -Pull ups- DAY 1");
+                    i1.putExtra("exoo1", exoo1);
+                    i1.putExtra("exoo2", exoo2);
+                    i1.putExtra("exoo3", exoo3);
+                    i1.putExtra("exoo4", exoo4);
+                    i1.putExtra("exoo5", exoo5);
+                    i1.putExtra("exoo6", exoo6);
+                    i1.putExtra("exoo7", exoo7);
+                    i1.putExtra("exoo8", exoo8);
+                    i1.putExtra("compteur_exo", compteur_exo);
+                    i1.putExtra("compteur_serie", compteur_serie);
+                    i1.putExtra("isCreated", isCreated);
+                    startActivity(i1);
                     break;
+
+                case R.id.workout2:
+                    exoo1=new RegularPullUps(3,10,60.0);
+                    exoo2=new CloseGripPullUps(3,10,60.0);
+                    exoo3=new WidePullUps(3,10,60.0);
+                    Intent i2 = new Intent(v.getContext(), ProgrammActivity.class);
+                    i2.putExtra("name_workout", "Armstrong -Pull ups- DAY 3");
+                    i2.putExtra("exoo1", exoo1);
+                    i2.putExtra("exoo2", exoo2);
+                    i2.putExtra("exoo3", exoo3);
+                    i2.putExtra("exoo4", exoo4);
+                    i2.putExtra("exoo5", exoo5);
+                    i2.putExtra("exoo6", exoo6);
+                    i2.putExtra("exoo7", exoo7);
+                    i2.putExtra("exoo8", exoo8);
+                    i2.putExtra("compteur_exo", compteur_exo);
+                    i2.putExtra("compteur_serie", compteur_serie);
+                    i2.putExtra("isCreated", isCreated);
+                    startActivity(i2);
+                    break;
+
+                case R.id.workout3:
+                    exoo1=new RegularPullUps(3,10,60.0);
+                    exoo2=new CloseGripPullUps(3,10,60.0);
+                    exoo3=new WidePullUps(3,10,60.0);
+                    Intent i3 = new Intent(v.getContext(), ProgrammActivity.class);
+                    i3.putExtra("name_workout", "Armstrong -Pull ups- DAY 3");
+                    i3.putExtra("exoo1", exoo1);
+                    i3.putExtra("exoo2", exoo2);
+                    i3.putExtra("exoo3", exoo3);
+                    i3.putExtra("exoo4", exoo4);
+                    i3.putExtra("exoo5", exoo5);
+                    i3.putExtra("exoo6", exoo6);
+                    i3.putExtra("exoo7", exoo7);
+                    i3.putExtra("exoo8", exoo8);
+                    i3.putExtra("compteur_exo", compteur_exo);
+                    i3.putExtra("compteur_serie", compteur_serie);
+                    i3.putExtra("isCreated", isCreated);
+                    startActivity(i3);
+                    break;
+                case R.id.workout4:
+                    exoo1=new CloseGripPullUps(20,10,60.0);
+                    Intent i4 = new Intent(v.getContext(), ProgrammActivity.class);
+                    i4.putExtra("name_workout", "Armstrong -Pull ups- DAY 1");
+                    i4.putExtra("exoo1", exoo1);
+                    i4.putExtra("exoo2", exoo2);
+                    i4.putExtra("exoo3", exoo3);
+                    i4.putExtra("exoo4", exoo4);
+                    i4.putExtra("exoo5", exoo5);
+                    i4.putExtra("exoo6", exoo6);
+                    i4.putExtra("exoo7", exoo7);
+                    i4.putExtra("exoo8", exoo8);
+                    i4.putExtra("compteur_exo", compteur_exo);
+                    i4.putExtra("compteur_serie", compteur_serie);
+                    i4.putExtra("isCreated", isCreated);
+                    startActivity(i4);
+                    break;
+
+
             }
 
         }};
